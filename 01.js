a75681c4a0a1622cafce4d709e3616bbe835e411
KaveriMetri07 <kaverimetri07@gmail.com>
2024-10-06 12:21:04 +0530

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,43 @@
+// let n = 1,
+//   min = 1,
+//   max = 1;
+// if (typeof n != "number" && max > min) {
+//   console.log("number " + n + " is invalid");
+// } else if (n > 0) {
+//   console.log("number " + n + " is positive");
+// } else if (n < 0) {
+//   console.log("number " + n + " is negative");
+// } else {
+//   console.log("number " + n + " is zero");
+// }
+
+// if (typeof n != "number") {
+//   console.log("number " + n + " is invalid");
+// } else if (n > min && n < max) {
+//   console.log("is in the range");
+// } else if (n === min && n == max) {
+//   console.log("is at the range");
+// } else {
+//   console.log("is outside the range");
+// }
+
+// let tip = 0;
+// calculateTip(40);
+// function calculateTip(billAmount) {
+//     if(typeof(billAmount) !="number" && billAmount)
+//   if (billAmount > 100) {
+//     tip = 0.25 * billAmount;
+//     console.log(tip);
+//   } else if (billAmount > 50 && billAmount < 100) {
+//     tip = 0.2 * billAmount;
+//     console.log(tip);
+//   } else {
+//     tip = 0.15 * billAmount;
+//     console.log(tip);
+//   }
+// }
+const y = {
+  name: "suraj",
+};
+let x = y.name[0];
+console.log(x);
